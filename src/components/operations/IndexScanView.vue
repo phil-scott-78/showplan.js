251d85935465007ddf8906d8bdc6674f44208aa6
Phil Scott <phil.scott@gmail.com>
2018-08-23 20:20:07 -0400

    fixes bug with seek predicates in index scans

--- src/components/operations/IndexScanView.vue
+++ src/components/operations/IndexScanView.vue
@@ -8,17 +8,9 @@
       </template>
     </list-or-div>
   </div>
-  <div v-if="indexScan.SeekPredicates !== undefined && indexScan.SeekPredicates.SeekPredicate !== undefined" class="content">
-    <h4>Seek Predicates</h4>
-    <list-or-div v-bind:data="indexScan.SeekPredicates.SeekPredicate">
-      <template slot-scope="{ item }">
-        <sql-string :sql="item.toString()"></sql-string>
-      </template>
-    </list-or-div>
-  </div>
   <div v-if="indexScan.SeekPredicates !== undefined && indexScan.SeekPredicates.SeekPredicateNew !== undefined" class="content">
     <h4>Seek Predicates</h4>
-    <list-or-div v-bind:data="indexScan.SeekPredicates.SeekPredicateNew">
+    <list-or-div v-if="indexScan.SeekPredicates.SeekPredicateNew !== undefined" v-bind:data="indexScan.SeekPredicates.SeekPredicateNew">
       <template slot-scope="{ item }">
         <list-or-div v-bind:data="item.SeekKeys">
           <template slot-scope="keyItem">
@@ -31,6 +23,23 @@
         </list-or-div>
       </template>
     </list-or-div>
+  </div>
+  <div v-if="indexScan.SeekPredicates !== undefined && indexScan.SeekPredicates.SeekPredicate !== undefined" class="content">
+    <h4>Seek Predicates</h4>
+    <list-or-div v-if="indexScan.SeekPredicates.SeekPredicate !== undefined" v-bind:data="indexScan.SeekPredicates.SeekPredicate">
+      <template slot-scope="{ item }">
+        <list-or-div v-bind:data="item.toStrings()">
+          <template slot-scope="keyString">
+            {{ keyString.item.key }} - <sql-string :sql="keyString.item.value"></sql-string>
+          </template>
+        </list-or-div>
+      </template>
+    </list-or-div>
+  </div>
+  <div class="content">
+    <ul class="stats">
+      <li>Ordered: <strong>{{ indexScan.Ordered }}</strong></li>
+    </ul>
   </div>
 </div>
 </template>
